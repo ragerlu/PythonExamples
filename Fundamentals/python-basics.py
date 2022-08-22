1c327f9736f04b9b8038fcc2bc8db72a32a026ae
ragerlu <ragerlu@udel.edu>
2022-08-22 18:36:46 -0400

    fill basics file.

---
+++ Fundamentals/python-basics.py
@@ -0,0 +1,65 @@
+#The purpose of the python-basics.py file is to provide examples of the basics of the R code language. 
+#The lines of code below can be typed into a Python Interactive Terminal to produce the desired result 
+#which is displayed in a comment line below in [].
+
+#Arithmetic: Operations based on arithmetic mathematic logic such as addition or subtraction.
+
+#Addition:
+1+1
+#[2]
+
+#Subtraction: 
+1-1
+#[0]
+
+#Division:
+2/1
+#[2]
+
+#Multiplication:
+2*2
+#[4]
+
+#Exponent Evaluation (Both methods):
+2**2
+2^2
+#[4]
+
+##########
+
+#Boolean: A logical data type that can have only the values true or false. Involving this
+#logic data type there is logic to be learned.
+
+#Boolean Values:
+True
+False
+#Now true can never equal false and false can never equal true. Think of these as a 
+#1 and a 0.
+
+#Logic operations for boolean evaluations:
+#Greater than:
+>
+#Less than:
+<
+#Greater than or equal to:
+>=
+#Less than or equal to:
+<=
+#Equal to:
+==
+#Not equal to:
+!=
+#And (Both):
+and 
+&&
+#Or (Both):
+or
+||
+
+#Examples of boolean operations are:
+True != False
+#[true]
+5>3
+#[true]
+True == False
+#[false]
